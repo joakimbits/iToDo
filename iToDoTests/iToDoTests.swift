3c065ca23ceb3606759a606d3229bb7b3c39ed1c
Joakim Pettersson <joakim.pettersson@join.se>
2024-11-21 17:43:34 +0100

    MacOS project without sources

---
+++ iToDoTests/iToDoTests.swift
@@ -0,0 +1,17 @@
+//
+//  iToDoTests.swift
+//  iToDoTests
+//
+//  Created by Joakim Pettersson on 2024-11-21.
+//
+
+import Testing
+@testable import iToDo
+
+struct iToDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
